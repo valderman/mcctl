c6b7f766751836c9e02b938a93f31d8d1a1d2633
Anton Ekblad <anton@ekblad.cc>
2014-10-25 01:23:42 +0200

    Fix build.

---
+++ MCCtl/Config/Parser.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE OverloadedStrings #-}
+-- | Parse instances from a YAML file.
+module MCCtl.Config.Parser where
+import Control.Applicative
+import qualified Data.ByteString as BS
+import Data.Yaml
+import MCCtl.Config
+
+-- | Parse an instance YAML file.
+parseInstance :: BS.ByteString -> Maybe Instance
+parseInstance bs = do
+  case decodeEither bs of
+    Right res -> flip parseMaybe res $ \o -> do
+      Instance <$> o .: "serverDirectory"
+               <*> o .:? "serverJAR" .!= "minecraft_server.jar"
+               <*> o .:? "autostart" .!= False
+               <*> o .:? "serverProperties"
+    _ -> do
+      Nothing
